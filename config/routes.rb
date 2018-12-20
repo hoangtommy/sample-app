96ed99c7796c2bebfe11a83f13c7e2392e3efc4e
Tommy <t@tommyhoang.com>
2018-12-20 13:47:27 -0800

    add test for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_page#home'
   get 'static_page/home'
-
   get 'static_page/help'
-  
   get 'static_page/about'
-
   get 'static_page/contact'
-
-  root 'application#hello'
-
 end
