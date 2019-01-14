0d6ed31009eed8ad17c250328e1149a17836280a
Tommy <t@tommyhoang.com>
2019-01-14 14:51:29 -0800

    add validation to creating new user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,7 +31,36 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@hogwarts.com"
     assert @user.invalid?
   end
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "email validation should accept valid emails" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid emails" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+      foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert @user.invalid?, "#{invalid_address} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert duplicate_user.invalid?
+  end
+
+  test "email addresses should be lowercased before saving" do
+    mixed_case_email = "sAmPLe@eXAmple.cOm"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
