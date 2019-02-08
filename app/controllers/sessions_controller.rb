df4daf8864988a4777e899113aa443d0fe8b6d86
Tommy <t@tommyhoang.com>
2019-02-08 13:37:05 -0800

    find and authenticate user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
+# Controller to manage log in sessions
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log users in and show their homepage
+    else
+      render 'new'
+    end
   end
+
+  def destroy; end
 end
