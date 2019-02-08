36788be758b7f05b95cb27cd9e7bc759a8b6f72c
Tommy <t@tommyhoang.com>
2019-02-08 15:21:19 -0800

    create temp session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # log users in and show their homepage
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'invalid email or password combination'
       render 'new'
