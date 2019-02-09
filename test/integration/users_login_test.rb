6c3ba7a183c64de9193d196a1603fea59bb35238
Tommy <t@tommyhoang.com>
2019-02-08 16:25:49 -0800

    add integration test for logged in nav bar

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  
+  def setup
+    @user = users(:tester)
+  end
+
   test "invalid login" do
     get login_path
     assert_template 'sessions/new'
@@ -12,11 +15,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  # test "valid login" do
-  #   get login_path
-  #   assert_template 'sessions/new'
-  #   post login_path, params: { sessions: { email: "test@test.com", password: "Hello123" } }
-  #   assert_template ''
-  #   assert_not flash.empty?
-  # end
+  test "valid login" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select 'a[href=?]', user_path(@user)
+  end
 end
