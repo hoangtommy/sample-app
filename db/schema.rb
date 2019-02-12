31263ec1f343ee4ce1da96b6d29695c3c6ba0a6d
Tommy <t@tommyhoang.com>
2019-02-11 16:56:05 -0800

    fix db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190211222631) do
+ActiveRecord::Schema.define(version: 20190212005234) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
