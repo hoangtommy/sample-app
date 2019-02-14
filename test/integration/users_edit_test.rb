80f6b3c8e5804b4b1f768d0a764fbcb6d4b1555e
Tommy <t@tommyhoang.com>
2019-02-14 15:54:22 -0800

    test integration for successful edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -11,11 +11,27 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   test "failed edit" do
     get edit_user_path(@user)
     assert_template 'users/edit'
-    patch user_path(@user), sparams: { user: { name: '',
+    patch user_path(@user), params: { user: { name: '',
                                               email: 'foo@none', 
                                               password: 'bar',
                                               password_confirmation: 'bor' } }
     assert_template 'users/edit'
     assert_select "div.alert", "The form contains 4 errors"
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = 'Foo Bar'
+    email = 'foo@bar.com'
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: '', 
+                                              password_confirmation: '' } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email                                             
   end
 end
