62bb6bf770d1ca1a93ce64d329ed9b5e79ba36f7
Tommy <t@tommyhoang.com>
2019-02-15 09:45:23 -0800

    restrict edit/update to correct user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
-
+  before_action :correct_user, only: [:edit, :update]
+    
   def new
     @user = User.new
   end
@@ -20,11 +21,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
+      # correct_user defines the user variable so we can remove
   end
 
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
+      # correct_user defines the user variable so we can remove
     if @user.update_attributes(user_params)
       flash[:success] = 'Profile updated'
       redirect_to @user
@@ -48,5 +51,10 @@ class UsersController < ApplicationController
        flash[:danger] = 'Please log in.'
        redirect_to login_url
      end
+   end
+
+   def correct_user
+     @user = User.find(params[:id])
+     redirect_to(root_url) unless current_user?(@user)
    end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:tester)
+    @user2 = users(:tester2)
   end
   
   test "should redirect edit when not logged in" do
@@ -21,6 +22,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get signup_path
     assert_response :success
+  end
+
+  test "should redirect edit when logged in as different user" do
+    log_in_as(@user2)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as different user" do
+    log_in_as(@user2)
+    patch user_path(@user), params: { user: { name: @user.name, 
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 
 end
