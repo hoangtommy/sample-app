1ebf7f7b6bc4f439d1d54829aefb3aa1b64beb62
Tommy <t@tommyhoang.com>
2019-02-27 15:23:11 -0800

    add create action and methods for password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :reset_token, :remember_token, :activation_token
     before_save :downcase_email #{ self.email = email.downcase }
     before_create :create_activation_digest
 
@@ -54,6 +54,17 @@ class User < ApplicationRecord
       UserMailer.account_activation(self).deliver_now
     end
 
+    def create_reset_digest
+      self.reset_token = User.new_token
+      update_attribute(:reset_digest, User.digest(reset_token))
+      update_attribute(:reset_sent_at, Time.zone.now)
+    end
+    
+    def send_password_reset_email
+      UserMailer.password_reset(self).deliver_now
+    end
+
+
     private
 
     # Converts email to lowercase
@@ -65,4 +76,5 @@ class User < ApplicationRecord
       self.activation_token = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
+
 end
