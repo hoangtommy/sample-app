f70c328e474aaeedd9e9d8c23d7d652b780b31cb
Tommy <t@tommyhoang.com>
2019-03-04 22:09:08 -0800

    update action to check password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -19,7 +20,19 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+  end
 
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = 'Password has been reset.'
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
@@ -34,5 +47,16 @@ class PasswordResetsController < ApplicationController
               @user.authenticated?(:reset, params[:id]))
         redirect_to root_url
       end
+    end
+
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = 'Password reset has expired.'
+        redirect_to new_password_reset_url
+      end
+    end
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
     end
 end
